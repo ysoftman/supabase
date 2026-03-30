160903adb7bee95d9601efb48bdf6f97efe5d245
ysoftman <ysoftman@gmail.com>
2026-03-30 22:25:43 +0900

    Add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -29,6 +29,7 @@ function getGitInfo() {
 const gitInfo = getGitInfo();
 
 export default defineConfig({
+  base: "/supabase/",
   define: {
     __LAST_VERSION_TAG__: gitInfo.LAST_GIT_TAG,
     __LAST_COMMIT_HASH__: gitInfo.LAST_GIT_COMMIT_HASH,
